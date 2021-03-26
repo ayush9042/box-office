2a06c7829fcb997b04bc4743a1bdf20c000f4eb6
Ayush <ayushaggarwal9042@gmail.com>
2021-03-26 23:00:53 +0530

    created styling page for title

--- src/components/Navs.js
+++ src/components/Navs.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
-
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { NavList, LinkStyled } from './Navs.styled';
 
 const LINK = [
   { to:"/", text:"Home" },
@@ -9,17 +9,18 @@ const LINK = [
 ];
 
 const Navs = () => {
+  const location = useLocation();
   return (
     <div>
-      <ul>
+      <NavList>
         {
           LINK.map(item => ( 
           <li key={item.to}>
-            <Link to={item.to}>{item.text}</Link>
+            <LinkStyled to={item.to} className={item.to === location.pathname ? 'active' : ''}>{item.text}</LinkStyled>
           </li>
           ))
         }
-      </ul>
+      </NavList>
     </div>
   )
 }
